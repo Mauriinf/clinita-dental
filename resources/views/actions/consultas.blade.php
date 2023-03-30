6569a4c35624a232e254ddacd4e187569858c7f1
mauriinf <mauricio_nina@hotmail.com>
2023-03-30 11:05:24 -0400

    botones curaciones

--- resources/views/actions/consultas.blade.php
+++ resources/views/actions/consultas.blade.php
@@ -1,15 +1,15 @@
 @role('Paciente')
-    <a class="btn btn-primary btn-sm" data-toggle="tooltip" title="Odontograma" href="{{ route('ver.odontograma',$id) }}"><i data-feather='x-square'></i></a>
+    <a class="btn btn-primary btn-sm" data-toggle="tooltip" title="Odontograma" href="{{ route('ver.odontograma',$id) }}">Odontograma</a>
 @else
     @can('odontograma')
-        <a class="btn btn-primary btn-sm" data-toggle="tooltip" title="Curaciones" href="{{ route('nuevo.odontograma',$id) }}"><i data-feather='x-circle'></i></a>
+        <a class="btn btn-primary btn-sm" data-toggle="tooltip" title="Curaciones" href="{{ route('nuevo.odontograma',$id) }}">Curacion</a>
     @endcan
 @endrole
 @can('editar-curacion')
-<button class="btn btn-warning btn-sm" data-toggle="tooltip" title="Editar" onclick="f_editar_consulta({{$id}},'{{$diagnostico}}','{{$medicamentos}}','{{$alergias}}','{{$enfermedades}}','{{$estado}}','{{$fecha_inicio}}','{{$fecha_final}}',{{$costo_total}},{{$id_paciente}},'{{$ci_paciente}}','{{$nom_paciente}}','{{$pat_paciente}}','{{$mat_paciente}}')"><i data-feather='edit'></i> </button>
+<button class="btn btn-warning btn-sm" data-toggle="tooltip" title="Editar" onclick="f_editar_consulta({{$id}},'{{$diagnostico}}','{{$medicamentos}}','{{$alergias}}','{{$enfermedades}}','{{$estado}}','{{$fecha_inicio}}','{{$fecha_final}}',{{$costo_total}},{{$id_paciente}},'{{$ci_paciente}}','{{$nom_paciente}}','{{$pat_paciente}}','{{$mat_paciente}}')">Editar</button>
 @endcan
-<a target="_blank" class="btn btn-danger btn-sm" data-toggle="tooltip" title="Historial Clinica" href="{{ route('reporte.historia_clinica', $id) }}"><i data-feather='clipboard'></i><i data-feather='file-text'></i></a>
+<a target="_blank" class="btn btn-danger btn-sm" data-toggle="tooltip" title="Historial Clinica" href="{{ route('reporte.historia_clinica', $id) }}">Historial</a>
 @role('Doctor|Admin|Asistente')
-<a target="_blank" class="btn btn-info btn-sm" data-toggle="tooltip" title="Recibo" href="{{ route('reporte.consulta.cobros', $id) }}"><i data-feather='file-text'></i></a>
+<a target="_blank" class="btn btn-info btn-sm" data-toggle="tooltip" title="Recibo" href="{{ route('reporte.consulta.cobros', $id) }}">Recibo</a>
 @endrole
 
--- resources/views/curaciones/index.blade.php
+++ resources/views/curaciones/index.blade.php
@@ -1,19 +1,20 @@
 @extends('vuexy.layouts.default', ['activePage' => 'curaciones'])
 @section('title','Consultas')
 @push('css-vendor')
-    <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/tables/datatable/dataTables.bootstrap5.min.css') !!}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/tables/datatable/responsive.bootstrap5.min.css') !!}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/tables/datatable/buttons.bootstrap5.min.css') !!}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/tables/datatable/rowGroup.bootstrap5.min.css') !!}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css') !!}">
-    <!-- END: Vendor CSS-->
-    <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/animate/animate.min.css') !!}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/extensions/sweetalert2.min.css') !!}">
-    <!-- END: Vendor CSS-->
-    <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="{!! asset('app-assets/css/plugins/extensions/ext-component-sweet-alerts.css') !!}">
+<!-- BEGIN: Vendor CSS-->
+<link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/tables/datatable/dataTables.bootstrap5.min.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/tables/datatable/responsive.bootstrap5.min.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/tables/datatable/buttons.bootstrap5.min.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/tables/datatable/rowGroup.bootstrap5.min.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/pickers/flatpickr/flatpickr.min.css') !!}">
+<!-- END: Vendor CSS-->
+<!-- BEGIN: Vendor CSS-->
+<link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/animate/animate.min.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('app-assets/vendors/css/extensions/sweetalert2.min.css') !!}">
+<!-- END: Vendor CSS-->
+<!-- BEGIN: Page CSS-->
+<link rel="stylesheet" type="text/css" href="{!! asset('app-assets/css/plugins/extensions/ext-component-sweet-alerts.css') !!}">
+<!-- END: Page CSS-->
 
 @endpush
 @section('content')
@@ -409,29 +410,28 @@
 @endsection
 
 @push('scripts-vendor')
-    <!-- BEGIN: Page Vendor JS-->
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/dataTables.bootstrap5.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/dataTables.responsive.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/responsive.bootstrap5.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/datatables.checkboxes.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/datatables.buttons.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/jszip.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/pdfmake.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/vfs_fonts.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/buttons.html5.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap5.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/buttons.print.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/dataTables.rowGroup.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/rowGroup.bootstrap5.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/tables/datatable/dataTables.select.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js') !!}"></script>
-    <!-- END: Page Vendor JS-->
-    <!-- BEGIN: Page JS-->
-    <script src="{!! asset('app-assets/vendors/js/extensions/sweetalert2.all.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/vendors/js/extensions/polyfill.min.js') !!}"></script>
-    <script src="{!! asset('app-assets/js/scripts/extensions/ext-component-sweet-alerts.js') !!}"></script>
-    <!-- END: Page JS-->
+     <!-- BEGIN: Page Vendor JS-->
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/dataTables.bootstrap5.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/dataTables.responsive.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/responsive.bootstrap5.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/datatables.checkboxes.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/datatables.buttons.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/jszip.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/pdfmake.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/vfs_fonts.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/buttons.html5.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap5.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/buttons.print.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/dataTables.rowGroup.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/rowGroup.bootstrap5.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/tables/datatable/dataTables.select.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js') !!}"></script>
+     <!-- END: Page Vendor JS-->
+     <!-- BEGIN: Page JS-->
+     <script src="{!! asset('app-assets/vendors/js/extensions/sweetalert2.all.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/vendors/js/extensions/polyfill.min.js') !!}"></script>
+     <script src="{!! asset('app-assets/js/scripts/extensions/ext-component-sweet-alerts.js') !!}"></script>
 
 
 @endpush
@@ -465,7 +465,7 @@ $(document).ready( function () {
                                 '<span class="badge bg-primary">CONCLUIDO</span>';
                         }
                     },
-                { data: 'botones', "orderable": false}
+                    { data: 'botones', "orderable": false}
         ],
         error: function(jqXHR, textStatus, errorThrown){
             $("#dt-ListaEspec").DataTable().clear().draw();
